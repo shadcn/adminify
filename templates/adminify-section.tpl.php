bf43b8bbad55b2cf99877bd0e2885b206b89b9bb
Arshad <arshad@chummun.com>
2014-03-27 20:54:22 +0400

    Add page template icon and make navigation collapsible

--- templates/adminify-section.tpl.php
+++ templates/adminify-section.tpl.php
@@ -4,17 +4,9 @@
  * Default theme implementation to display an adminify section.
  */
 ?>
-<a href="#">
-  <?php if ($icon): ?>
-    <i class="icon fa fa-<?php print $icon; ?>"></i>
-  <?php endif; ?>
-  <?php print $title; ?>
-  <?php if (count($pages)): ?>
-    <i class="arrow fa fa-angle-right pull-right"></i>
-  <?php endif; ?>
-</a>
+<?php print $link; ?>
 <?php if (count($pages)): ?>
-  <ul class="section-pages">
+  <ul id="<?php print $section_id; ?>" class="section-pages collapse">
     <?php foreach ($pages as $page): ?>
       <li><?php print $page; ?></li>
     <?php endforeach; ?>
